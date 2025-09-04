18c93ac61e2193cbe8812fb45bfd6d6da67fdd44
Diane Bury <diane.bury@utoulouse.fr>
2025-09-04 11:50:28 +0200

    add files from Martin Mujica repository

---
+++ motoman_hc10_moveit_config/scripts/create_trajectory.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+import numpy as np
+import csv
+
+def moveJ(qi,qf,tf,Te):
+# This function calculate the different angular values
+# INPUT: initial articulations values, final articulations values, total time, step time
+# OUTPUT: matrix 6x20
+    l1=np.array(([1,0,0,0]))
+    l2=np.array(([0,1,0,0]))
+    l3=np.array(([1,tf,tf**2,tf**3]))
+    l4=np.array(([0,1,2*tf,3*(tf**2)]))
+
+    A = np.array(l1)
+    A = np.vstack((A,l2))
+    A = np.vstack((A,l3))
+    A = np.vstack((A,l4))
+    A = np.linalg.inv(A)
+
+    for j in range (round(tf/Te)) : 
+        for i in range (6) :
+            Q = np.array(([qi[i]],[0],[qf[i]],[0]))
+            C = np.matmul(A,Q)
+
+            a = C[0] + C[1]*Te*j + C[2]*((Te*j)**2) + C[3]*((Te*j)**3)
+            if (i == 0) :  
+                b = np.array(a)
+            else :
+                b = np.append(b,a)
+        if (j == 0):
+            q = np.array(b)
+        else :
+            q = np.vstack((q,b))
+    return q
+qi=[0.4419472813606262, 0.13577520847320557, 1.0137178897857666, -0.7563529014587402, -0.2653222978115082, 0.03390097618103027]
+qf = np.copy(qi)
+qf[0] = qf[0]+0.34 
+q = moveJ(qi,qf,5,0.1)
+print (q)
+
+with open('/home/felix/catkin_ws_mm2/src/ros_yaskawa_hc10/motoman_hc10_moveit_config/config/trajectory.csv','w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for i in range(len(q)):
+    	csv_writer.writerow(q[i])
+
---
+++ motoman_hc10_moveit_config/scripts/execute_trajectory.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python
+from os import wait
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import csv
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('execute_trajectory',anonymous=True)
+current_joint_position = [0,0,0,0,0,0]
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.position)
+    global current_joint_position
+    current_joint_position = data.position
+rospy.Subscriber("joint_states", JointState, callback)
+pub = rospy.Publisher('joint_path_command', JointTrajectory, queue_size=10)
+
+def is_current_init(initial_position):
+    global current_joint_position
+    for i in range(len(initial_position)):
+        if ((initial_position[i]-current_joint_position[i])>0.01):
+            return False
+    return True
+    
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group = moveit_commander.MoveGroupCommander("manipulator")
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
+                                               
+                                               
+                                               
+trajectory = JointTrajectory()
+trajectory.joint_names = ["joint_1_s","joint_2_l","joint_3_u","joint_4_r","joint_5_b","joint_6_t"]
+
+
+
+
+with open('/home/felix/Desktop/SAE_IUT/trajectory.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 1
+    Ts = 0.1
+    for row in csv_reader:
+        point = JointTrajectoryPoint()
+        point.positions = list(map(float,row))
+        point.velocities = [0,0,0,0,0,0]
+        point.time_from_start = rospy.Duration(Ts*line_count)
+        trajectory.points.append(point)
+        #trajectory.append(list(map(float,row)))
+        #print(row)
+        line_count += 1
+            
+initial_position = trajectory.points[0].positions
+init_ok = is_current_init(initial_position) # is the first point of the trajectory (almost) equal tu current point?
+print (initial_position)
+if (init_ok == False):
+    print("Fisrt going to initial position via moveit")
+    group.set_joint_value_target(initial_position)
+    group.go(wait=True) #Blocking call, same as "group.move()" for roscpp
+    group.stop()
+    rospy.sleep(3)
+
+point_current = JointTrajectoryPoint()
+point_current.positions = list(current_joint_position)
+point_current.velocities = [0,0,0,0,0,0]
+point_current.time_from_start = rospy.Duration(0)
+trajectory.points.insert(0, point_current)
+
+#print (len(trajectory))
+trajectory.header.stamp = rospy.Time.now()
+trajectory.header.seq = 1
+pub.publish(trajectory)
+print (trajectory)
+
+
+#Planning and executing with set_joint_value_target
+
+"""for n in range(len(trajectory)):
+    if not rospy.is_shutdown():
+        group.set_joint_value_target(trajectory[n])
+        print("New target has been set")
+        #plan2 = group.plan()
+        print("Plannig done, now executing \n")
+        group.go(wait=True) #Blocking call, same as "group.move()" for roscpp
+        group.stop()
+
+moveit_commander.roscpp_shutdown()
+"""
+
+
+
+
+
+
