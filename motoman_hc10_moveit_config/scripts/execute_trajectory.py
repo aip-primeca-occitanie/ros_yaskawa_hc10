1f86aae7ffb3624ddd7d19b21e32d5caf98aa204
Diane Bury <diane.bury@utoulouse.fr>
2025-09-08 11:01:49 +0200

    Update script execute_trajectory to Python3

--- motoman_hc10_moveit_config/scripts/execute_trajectory.py
+++ motoman_hc10_moveit_config/scripts/execute_trajectory.py
@@ -22,13 +22,13 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
 group_state = group.get_named_targets()
 
 #Planning and executing with set_joint_value_target
-print "Number of group states in srdf file: %i \n" % len(group_state)
+print("Number of group states in srdf file: %i \n" % len(group_state))
 for n in range(0,len(group_state)): #Home configuration (i.e 0 position) is a singularity 
     if not rospy.is_shutdown():
-        print "group state %i: %s" %(n,group_state[n])
-        print "Joint Values %s" %group.get_named_target_values(group_state[n])
+        print("group state %i: %s" %(n,group_state[n]))
+        print("Joint Values %s" %group.get_named_target_values(group_state[n]))
         group.set_joint_value_target(group.get_named_target_values(group_state[n]))
-        print "New target has been set"
+        print("New target has been set")
         plan2 = group.plan()
         rospy.sleep(1)
 #If you want to move the group to the specified targets uncomment the lines below
